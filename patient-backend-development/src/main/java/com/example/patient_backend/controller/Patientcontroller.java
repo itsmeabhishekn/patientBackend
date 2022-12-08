90142909446717baa857a2facadeaaf5f77550e5
itsmeabhishekn <abhishekn4454@gmail.com>
2022-12-08 14:19:24 +0530

    added search and delete

--- patient-backend-development/src/main/java/com/example/patient_backend/controller/Patientcontroller.java
+++ patient-backend-development/src/main/java/com/example/patient_backend/controller/Patientcontroller.java
@@ -6,18 +6,25 @@ import com.example.patient_backend.model.Patientmodel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
 public class Patientcontroller {
     @Autowired
     private Patientdao dao;
+
+
     @CrossOrigin(origins ="*")
     @GetMapping("/viewall")
-    public List<Patientmodel> Viewall()
+    public List<Patientmodel> viewall()
     {
         return (List<Patientmodel>) dao.findAll();
     }
+
+
+
+
     @CrossOrigin(origins ="*")
     @PostMapping(path="/add",consumes = "application/json",produces = "application/json")
     public String add(@RequestBody Patientmodel p)
@@ -32,4 +39,27 @@ public class Patientcontroller {
         dao.save(p);
         return "Patient data added Successfully";
     }
+
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/search",consumes = "application/json",produces = "application/json")
+    public List<Patientmodel> search(@RequestBody Patientmodel s)
+    {
+        String id=String.valueOf(s.getId());
+        System.out.println(id);
+        return (List<Patientmodel>) dao.search(s.getId());
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/delete")
+    public HashMap<String, String> delete(@RequestBody Patientmodel d )
+    {
+        String id = String.valueOf(d.getId());
+        System.out.println(id);
+        dao.delete(d.getId());
+        HashMap<String,String> map = new HashMap<>();
+        map.put("status","success");
+        return map;
+    }
+
 }
--- patient-backend-development/src/main/java/com/example/patient_backend/dao/Patientdao.java
+++ patient-backend-development/src/main/java/com/example/patient_backend/dao/Patientdao.java
@@ -1,8 +1,23 @@
 package com.example.patient_backend.dao;
 
 import com.example.patient_backend.model.Patientmodel;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface Patientdao extends CrudRepository<Patientmodel,Integer>
 {
+
+    @Query(value = "SELECT `id`, `address`, `date`, `doctor`, `number`, `url`, `username` FROM `patient` WHERE `id` = :id",nativeQuery = true)
+    List<Patientmodel> search(@Param("id") Integer id);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `patient` WHERE `id` = :id",nativeQuery = true)
+    void delete(@Param("id") Integer id);
 }
